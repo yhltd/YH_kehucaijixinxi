33c771329131b2f5f4bd7cb419570e116d74b75a
yhocn107 <yhocn107@163.com>
2023-05-12 18:47:13 +0800

    lvshanyuan 20230512 up
    1.表单基础组件：按钮
    2.表单基础组件：商品
    3.表单基础组件：排序

--- code/wb/WeBank/src/main/java/servlet/User.java
+++ code/wb/WeBank/src/main/java/servlet/User.java
@@ -1,28 +1,28 @@
-package servlet;
-
-public class User {
-    Private String username;
-    Private String password;
-
-    public User(Private string, Private string1) {
-        String = string;
-        String = string1;
-    }
-
-    public Private getString() {
-        return String;
-    }
-
-    public void setString(Private string) {
-        String = string;
-    }
-
-    @Override
-    public java.lang.String toString() {
-        return "User{" +
-                "String=" + String +
-                ", String=" + String +
-                '}';
-    }
-
-}
+//package servlet;
+//
+//public class User {
+//    Private String username;
+//    Private String password;
+//
+//    public User(Private string, Private string1) {
+//        String = string;
+//        String = string1;
+//    }
+//
+//    public Private getString() {
+//        return String;
+//    }
+//
+//    public void setString(Private string) {
+//        String = string;
+//    }
+//
+//    @Override
+//    public java.lang.String toString() {
+//        return "User{" +
+//                "String=" + String +
+//                ", String=" + String +
+//                '}';
+//    }
+//
+//}
